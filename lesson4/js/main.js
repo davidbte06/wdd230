3352b21aa3ebd9f1945bff2e5b6f2890b569c0f4
davidbte06 <davidbte06@gmail.com>
2021-10-09 10:10:57 -0500

    added preston home page

---
+++ lesson4/js/main.js
@@ -0,0 +1,20 @@
+document.querySelector("#copyrightyear").textContent = new Date().getFullYear();;
+
+let event = new Date();
+
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+
+let date = event.toLocaleDateString('en-UK', options);
+
+document.querySelector("#lastupdated").textContent = date;
+
+
+function toggleMenu() {
+
+    document.querySelector("#menu").classList.toggle("main_menu");
+}

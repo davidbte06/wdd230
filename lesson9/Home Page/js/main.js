57d4b69241fbd4ec50242602e570dd0430809164
davidbte06 <davidbte06@gmail.com>
2021-11-13 11:10:36 -0600

    assignment w09

---
+++ lesson9/Home Page/js/main.js
@@ -0,0 +1,20 @@
+// Current Date
+const date = new Date(Date.now())
+
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+
+document.querySelector('#lastupdated').textContent = date.toLocaleDateString('en-Us', options);
+
+// Copyrigth Year
+document.querySelector('#copyrightyear').textContent = new Date().getFullYear();
+
+// Navegation Bar
+function toggleMenu() {
+
+    document.querySelector("#menu").classList.toggle("main_menu");
+}

747493ca572ef22cf126479698379c77b5886a25
davidbte06 <davidbte06@gmail.com>
2021-11-24 22:01:48 -0600

    finished gallery

--- lesson11/js/main.js
+++ lesson11/js/main.js
@@ -57,12 +57,73 @@ fetch(urlPreston)
 
   .then(function (jsonPreston) {
 
-      const towns = jsonPreston['towns'];
-      const townPreston = towns.filter((towns) => towns.name === 'Preston');
+    const towns = jsonPreston['towns'];
+    const townPreston = towns.filter((towns) => towns.name === 'Preston');
 
-      townPreston.forEach((xPreston) => {
-        document.getElementById('preston-events').innerHTML = `${xPreston.events.join('<br>')}`;
+    townPreston.forEach((xPreston) => {
+      document.getElementById('preston-events').innerHTML = `${xPreston.events.join('<br>')}`;
 
-      });
+    });
 
   });
+
+// GALLERY 
+// Lazy Loading
+const images = document.querySelectorAll('img[data-src]');
+
+const imageOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px 50px 0px'
+};
+
+function loadImages(image) {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    (image.removeAttribute('data-src'))
+  };
+}
+
+if ('IntersectionObserver' in window) {
+  const imageObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imageOptions);
+
+  images.forEach((img) => {
+    imageObserver.observe(img);
+  });
+} else {
+  images.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+// Local Storage
+const daysSinceLastVisit = document.querySelector('#last-visited');
+
+function setDateLastVisited() {
+  localStorage.setItem('dateLastVisited', date);
+}
+
+function getDateLastVisited(daysSinceLastVisit) {
+  try {
+    const milisecondsToDays = 8640000;
+    const lastVisit = localStorage.getItem('dateLastVisisted') || date;
+    const days = Math.round((lastVisit - date) / milisecondsToDays);
+
+    if (days > 0) {
+      daysSinceLastVisit.textContent = days;
+    } else {
+      daysSinceLastVisit.textContent = 0;
+    }
+  } catch (er) {
+    daysSinceLastVisit.textContent = 0;
+  }
+}
+
+getDateLastVisited(daysSinceLastVisit)
+setDateLastVisited()

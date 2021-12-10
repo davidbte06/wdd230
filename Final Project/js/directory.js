54cd30ee9efa7718f601c0b50b79ca7ffd258b72
davidbte06 <davidbte06@gmail.com>
2021-12-10 12:47:34 -0600

    fixed errors

--- Final Project/js/directory.js
+++ Final Project/js/directory.js
@@ -15,30 +15,30 @@ const directory = 'json/directory.json';
 fetch(directory).then((response) => response.json()).then((jsonObject) => {
 
     //Business
-    Object.keys(jsonObject.organizations).forEach(i => {
+    Object.keys(jsonObject.business).forEach(i => {
         let div = document.createElement('div');
         div.classList.add('organization-div');
 
         let name = document.createElement('h3');
 
-        if (jsonObject.organizations[i].logo != "" || jsonObject.organizations[i].website != undefined) {
+        if (jsonObject.business[i].logo != "" || jsonObject.business[i].website != undefined) {
             let logo = document.createElement('img');
-            logo.setAttribute('src', jsonObject.organizations[i].logo);
-            logo.setAttribute('alt', `${jsonObject.organizations[i].name} logo`);
+            logo.setAttribute('src', jsonObject.business[i].logo);
+            logo.setAttribute('alt', `${jsonObject.business[i].name} logo`);
 
             div.appendChild(logo);
         }
 
         let address = document.createElement('p');
-        address.textContent = jsonObject.organizations[i].address;
+        address.textContent = jsonObject.business[i].address;
 
         let phone = document.createElement('p');
-        phone.textContent = jsonObject.organizations[i].phone;
+        phone.textContent = jsonObject.business[i].phone;
 
-        if (jsonObject.organizations[i].website != "" || jsonObject.organizations[i].website != undefined) {
+        if (jsonObject.business[i].website != "" || jsonObject.business[i].website != undefined) {
             let website = document.createElement('a');
-            website.setAttribute('href', jsonObject.organizations[i].website);
-            website.textContent = jsonObject.organizations[i].name;
+            website.setAttribute('href', jsonObject.business[i].website);
+            website.textContent = jsonObject.business[i].name;
 
             div.appendChild(name);
             name.appendChild(website);
